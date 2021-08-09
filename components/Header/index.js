2278cdb023f17bdb568148ff1928d70f81f63042
Victor <victor@Victors-iMac-Pro.local>
2021-08-09 12:50:56 -0300

    header done

--- components/Header/index.js
+++ components/Header/index.js
@@ -1,11 +1,18 @@
-import {
-  Background
-} from './styles';
+/* eslint-disable @next/next/no-img-element */
+import { Container } from '../grid';
+import { Background } from './styles';
 
 export default function Header() {
   return (
     <Background>
-      <p>Header</p>
+      <Container className='items-center py-24'>
+        <div className='col-span-6'>
+          <img src='/logo.png' alt='Super Sorteios' />
+        </div>
+        <div className='col-span-6'>
+          <img src='/iphone.png' alt='iPhone 12 128GB' />
+        </div>
+      </Container>
     </Background>
   );
 }
--- components/Header/styles.js
+++ components/Header/styles.js
@@ -2,5 +2,4 @@ import styled from 'styled-components';
 
 export const Background = styled.div.attrs({})`
   background-image: radial-gradient(circle at 0 0, #4585FF, #00278A);
-  height: 400px;
 `;
