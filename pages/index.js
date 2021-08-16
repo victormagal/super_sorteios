fa44b0405ee916af961cd4927084f283d50e2154
Victor <victor@Victors-iMac-Pro.local>
2021-08-15 23:57:35 -0300

    facebook pixel added

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,5 @@
+/* eslint-disable @next/next/no-img-element */
+/* eslint-disable jsx-a11y/alt-text */
 import router from 'next/router';
 import Head from 'next/head';
 import { Field, Form, Formik } from 'formik';
@@ -29,6 +31,30 @@ export default function Home() {
         <title>Super Sorteios</title>
         <meta name="description" content="Super Sorteios" />
         <link rel="icon" href="/favicon.ico" />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '419294856155990');
+              fbq('track', 'PageView');
+            `
+          }}
+        />
+        <noscript>
+          <img 
+            height="1"
+            width="1" 
+            style={{ display: 'none' }} 
+            src={`https://www.facebook.com/tr?id=419294856155990&ev=PageView&noscript=1`}
+          />
+        </noscript>
       </Head>
       <main>
         <Header />
